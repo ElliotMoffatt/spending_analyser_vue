13e176711a2ac96ae4663b748533b67127b8f11f
munkfish <munkf@MUNKFISHTOWER>
2023-01-07 16:19:05 +0000

    deploy

--- dist/js/about.3fdfcb86.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,a){"use strict";a.r(t);var o=a("7a23"),e={class:"about"},r=Object(o["h"])("p",null,"You can download your transaction history from Santander as a .txt file, then upload it here to analyse your spending habits",-1);function s(n,t){return Object(o["q"])(),Object(o["d"])("div",e,[r])}var u=a("d959"),c=a.n(u);const d={},i=c()(d,[["render",s]]);t["default"]=i}}]);
-//# sourceMappingURL=about.3fdfcb86.js.map
